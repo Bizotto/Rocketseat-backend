a68fd65cd7e45b3cdd9bd9af62d757d645ca91c3
Bizotto <bizotto.nicolas@gmail.com>
2022-08-09 13:32:14 -0300

    fix label

--- src/controllers/ProfileUserController.ts
+++ src/controllers/ProfileUserController.ts
@@ -1,26 +1,14 @@
+import { Request, Response } from 'express';
+import { ProfileUserService } from '../services/ProfileUserService';
 
+class ProfileUserController {
+  async handle(request: Request, response: Response) {
+    const { user_id } = request;
+    const service = new ProfileUserService();
+    const result = await service.execute(user_id);
 
-
-
-
-
-import { Request, Response } from "express";
-import { ProfileUserSercice } from "../services/ProfileUserService";
-
-
-
-class ProfileUserController  {
-    async handle(request: Request, response: Response) {
-
-            const { user_id } = request;
-            const service = new ProfileUserSercice();
-            const result = await service.execute(user_id);
-
-            return response.json(result);
-
-
-    }
+    return response.json(result);
+  }
 }
 
-
-export { ProfileUserController }
+export { ProfileUserController };
--- src/services/ProfileUserService.ts
+++ src/services/ProfileUserService.ts
@@ -1,19 +1,15 @@
+import prismaClient from '../prisma';
 
-import prismaClient from "../prisma"
+class ProfileUserService {
+  async execute(user_id: string) {
+    const user = await prismaClient.users.findFirst({
+      where: {
+        id: user_id,
+      },
+    });
 
-
-class ProfileUserSercice {
-    async execute(user_id:string) {
- const user = await prismaClient.users.findFirst({
-     where: {
-         id: user_id    
-     }
- })
-    
-return user
-    }
-
+    return user;
+  }
 }
 
-
-export { ProfileUserSercice }
+export { ProfileUserService };
